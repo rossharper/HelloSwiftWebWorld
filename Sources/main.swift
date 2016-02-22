11e55ec2bb600b2a7cb8dd2f7a24e14624e24e61
Ross Harper <ross@rossharper.net>
2016-02-22 22:14:45 +0000

    specify port on command line

--- Sources/main.swift
+++ Sources/main.swift
@@ -11,9 +11,20 @@ import Vapor
 
 print("HelloSwiftWebWorld -- starting...")
 
+var port : Int = 4242
+
+for arg in Process.arguments {
+    if arg.hasPrefix("port=") {
+        let portArg : Int? = Int(arg.substringFromIndex(arg.startIndex.advancedBy(5)))
+        if portArg != nil {
+            port = portArg!
+        }
+    }
+}
+
 Route.get("/") { _ in
     return "<h1>Hello Swift Web World</h1><p>Hello, I am a web server written in <a href=\"https://swift.org/\" target=\"_blank\">Swift</a>, running on a Linux virtual machine in the cloud.</p>"
 }
 
 let server = Server()
-server.run(port: 4242)
+server.run(port: port)
