1efcc084bbffb16eebf8c365a0881d7afc0dd6f1
Ross Harper <ross@rossharper.net>
2016-02-22 21:48:30 +0000

    updated copy

--- Sources/main.swift
+++ Sources/main.swift
@@ -12,8 +12,8 @@ import Vapor
 print("HelloSwiftWebWorld -- starting...")
 
 Route.get("/") { _ in
-    return "Hello Swift Web World"
+    return "<h1>Hello Swift Web World</h1><p>Hello, I am a web server written in <a href=\"https://swift.org/\" target=\"_blank\">Swift</a>, running on a Linux virtual machine in the cloud.</p>"
 }
 
 let server = Server()
-server.run(port: 80)
+server.run(port: 4242)
