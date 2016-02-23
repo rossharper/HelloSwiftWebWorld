7bc7c03391de0847d7f8d6abb8857bb9aee0646a
Ross Harper <ross@rossharper.net>
2016-02-23 23:35:11 +0000

    Fucking. Horrible. Code.
    Vapor isn't ready for the big time. Doing asyng web service requests is a bit of a pain.
    My swift code is a mess. There's got to be a cleaner way to do the JSON parsing and deal with the optionals.

--- Sources/main.swift
+++ Sources/main.swift
@@ -27,8 +27,97 @@ View.renderers[".stencil"] = StencilRenderer()
 
 let app = Application()
 
+func parseResponse(data: NSData?, error: NSError?) -> NSDictionary {
+    guard let responseData = data else {
+        print("Error: did not receive data")
+        return NSDictionary()
+    }
+    guard error == nil else {
+        print("error calling GET on /posts/1")
+        print(error)
+        return NSDictionary()
+    }
+    // parse the result as JSON, since that's what the API provides
+    do {
+        return try NSJSONSerialization.JSONObjectWithData(responseData,
+                                                             options: []) as! NSDictionary
+    } catch  {
+        print("error trying to convert data to JSON")
+        return NSDictionary()
+    }
+}
+
+struct Episode {
+    var title : String
+    var subtitle : String
+    var imageUrl : String
+}
+
+func topTen(iblData: NSDictionary) -> String {
+    let ge = iblData.objectForKey("group_episodes") as! NSDictionary?
+    
+    if ge == nil {
+        print("no group_episodes")
+        return ""
+    }
+    
+    let episodeDictArray = ge!.objectForKey("elements") as! NSArray?
+    
+    if episodeDictArray == nil {
+        print("no elements")
+        return ""
+    }
+    
+    let allEpisodes : [Episode] = episodeDictArray!.map{ (value) -> Episode in
+        let episodeDict = value as! NSDictionary
+        let episodeTitle = episodeDict.objectForKey("title") as! String?
+        let episodeSubtitle = episodeDict.objectForKey("subtitle") as! String?
+        let episodeImageUrl = (episodeDict.objectForKey("images") as! NSDictionary?)?.objectForKey("standard") as! String?
+        let title = episodeTitle ?? ""
+        let subtitle = episodeSubtitle ?? ""
+        let imageUrl = (episodeImageUrl ?? "").stringByReplacingOccurrencesOfString("{recipe}", withString: "240x135")
+        return Episode(title: title, subtitle: subtitle, imageUrl: imageUrl)
+        }
+        
+    var noEastEnders : [Episode] = allEpisodes.filter {
+            !$0.title.lowercaseString.hasPrefix("eastenders")
+        }
+    
+    while noEastEnders.count > 10 {
+        noEastEnders.removeLast()
+    }
+    
+    let tablerows : String = noEastEnders.map { (value) -> String in
+            return "<tr><td><img src=\"" + value.imageUrl + "\"/></td><td>" + value.title + "<br/>" + value.subtitle + "</td></tr>"
+        }.reduce("", combine:{$0 + $1})
+    
+    // constructing a string because the fucking templating framework doesnt work
+    return "<table><thead><tr><th colspan=\"2\">iPlayer Top 10 Episodes that aren't EastEnders</th></tr></thead><tbody>" + tablerows + "</tbody></table>"
+}
+
 app.get("/") { request in
-    return try View(path: "index.stencil")
+    guard let url = NSURL(string: "http://ibl.api.bbci.co.uk/ibl/v1/groups/popular/episodes?per_page=40") else {
+        print("Error: cannot create URL")
+        return "ERRORRRRR"
+    }
+    let urlRequest = NSURLRequest(URL: url)
+    
+    let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+    
+    let dataTask = session.dataTaskWithRequest(urlRequest)
+    
+    var iblData : NSDictionary = NSDictionary()
+    
+    // Do nasty synchronous request because Vapor async handling is horrible
+    let semaphore: dispatch_semaphore_t = dispatch_semaphore_create(0)
+    let task = session.dataTaskWithRequest(urlRequest, completionHandler: { (data, response, error) in
+        iblData = parseResponse(data, error: error)
+        dispatch_semaphore_signal(semaphore);
+    })
+    task.resume()
+    dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
+    
+    return try View(path: "index.stencil", context: ["episodes": topTen(iblData)])
 }
 
 app.start(port: port)
